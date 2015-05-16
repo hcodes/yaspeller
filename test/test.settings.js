016c712e9d4b61003ad3f16bc6752202beb5d407
Denis Seleznev <hcodes@yandex.ru>
2015-05-16 18:28:30 +0300

    Аbility to specify multiple dictionaries in option --dictionary #33

---
+++ test/test.settings.js
@@ -0,0 +1,78 @@
+var yaspeller = require('../lib/yaspeller'),
+    assert = require('chai').assert,
+    fs = require('fs'),
+    getFile = function(name) {
+        return fs.readFileSync(name).toString('utf-8');
+    };
+
+describe('Settings', function() {
+    this.timeout(10000);
+    
+    it('checkYo', function(done) {
+        yaspeller.checkText('Елка, елки, еж.', function(err, data) {
+            assert.equal(err, false);
+            assert.equal(data.length, 2);
+            done();
+        }, {lang: 'ru', checkYo: true});
+    });
+
+    it('ignoreTags off', function(done) {
+        var text = getFile('./test/texts/settings_ignore_tags.txt');
+        yaspeller.checkText(text, function(err, data) {
+            assert.equal(err, false);
+            assert.equal(data.length, 4);
+            done();
+        }, {lang: 'en', format: 'html', ignoreTags: []});
+    });
+
+    it('ignoreTags on', function(done) {
+        var text = getFile('./test/texts/settings_ignore_tags.txt');
+        yaspeller.checkText(text, function(err, data) {
+            assert.equal(err, false);
+            assert.equal(data.length, 1);
+            done();
+        }, {lang: 'en', format: 'html', ignoreTags: ['code']});
+    });
+
+    it('Without lang and format', function(done) {
+        yaspeller.checkText('<coddeeee> maasjedqjw  уфокцошцуок', function(err, data) {
+            assert.equal(err, false);
+            assert.equal(data.length, 2);
+            done();
+        });
+    });
+
+    it('Array of langs', function(done) {
+        var text = getFile('./test/texts/settings_array_langs.txt');
+        yaspeller.checkText(text, function(err, data) {
+            assert.equal(err, false);
+            assert.equal(data.length, 2);
+            done();
+        }, {lang: ['ru']});
+    });
+
+    it('Unknown format', function(done) {
+        yaspeller.checkText('<coddeeee> maasjedqjw  уфокцошцуок', function(err, data) {
+            assert.equal(err, false);
+            assert.equal(data.length, 3);
+            done();
+        }, {format: 'unknown'});
+    });
+
+    it('extname .htm', function(done) {
+        yaspeller.checkText('&quot;Москва&quot;', function(err, data) {
+            assert.equal(err, false);
+            assert.equal(data.length, 0);
+            done();
+        }, {extname: '.htm'});
+    });
+
+    it('Format markdown', function(done) {
+        var text = getFile('./test/texts/settings_markdown.txt');
+        yaspeller.checkText(text, function(err, data) {
+            assert.equal(err, false);
+            assert.equal(data.length, 0);
+            done();
+        }, {ignoreTags: ['code', 'pre']});
+    });
+});
