58658104bd3201f9ecc1bcb264d5d8fc909bdb0a
Denis Seleznev <hcodes@yandex.ru>
2018-06-25 16:13:24 +0300

    Tests

--- test/test.config.js
+++ test/test.config.js
@@ -1,14 +1,18 @@
 const assert = require('chai').assert;
+
 const sinon = require('sinon');
+const sandbox = sinon.createSandbox();
+
 const config = require('../lib/config');
 const exitCodes = require('../lib/exit-codes');
 
 beforeEach(function() {
-    sinon.stub(process, 'exit');
+    sandbox.stub(process, 'exit');
+    sandbox.spy(console, 'error');
 });
 
 afterEach(function() {
-    process.exit.restore();
+    sandbox.restore();
 });
 
 describe('Config', function() {
@@ -23,5 +27,19 @@ describe('Config', function() {
     it('get, throw', function() {
         config.get('test/json/error_parsing.json');
         assert.equal(process.exit.args[0], exitCodes.ERROR_CONFIG);
+    });
+
+    it('get, unknown properties', function() {
+        config.get('test/json/unknown_properties.json');
+        
+        const count = console.error.args.length;
+        assert.equal(count, 2);
+    });
+
+    it('get, wrong property type', function() {        
+        config.get('test/json/wrong_prop_type.json');
+
+        const count = console.error.args.length;
+        assert.equal(count, 2);
     });
 });
